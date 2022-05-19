0c68183aa348e0997471385491d2f90a0c297b32
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-05-18 23:30:08 -0400

    Replace hardcoded movie data with API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -14,11 +14,7 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Alien', Description: 'The crew of a commercial spacecraft encounter a deadly lifeform after investigating an unknown transmission.', ImagePath: 'https://www.imdb.com/title/tt0078748/mediaviewer/rm2032147969/' },
-        { _id: 2, Title: 'Prometheus', Description: 'Following clues to the origin of mankind, a team finds a structure on a distant moon, but they soon realize they are not alone.', ImagePath: 'https://www.imdb.com/title/tt1446714/mediaviewer/rm430486784/' },
-        { _id: 3, Title: 'Starship Troopers', Description: 'Humans in a fascist, militaristic future wage war with giant alien bugs.', ImagePath: 'https://www.imdb.com/title/tt0120201/mediaviewer/rm701657344/' }
-      ],
+      movies: [],
       // determine if a movie was clicked for details. 
       selectedMovie: null
     };
