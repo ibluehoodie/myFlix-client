81ef4a19d3df6ada2eb93a4a8b9e6ef60a4a440d
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-06-04 09:12:50 -0400

    3.6 WIP export ProfileView - NEEDS WORK

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row, Modal } from 'react-bootstrap';
+import { MovieCard } from '../movie-card/movie-card';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+export class ProfileView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      Username: null,
+    }
+  }
+
+
+}
