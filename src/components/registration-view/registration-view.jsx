cba7d3749174e15b8cc865317b9f77c0cec0d04c
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-06-03 21:37:47 -0400

    3.6 Declare hooks for RegistrationView inputs

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -9,6 +9,12 @@ export function RegistrationView(props) {
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
+  // Declare hook for each input
+  const [usernameErr, setUsernameErr] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+  const [emailErr, setEmailErr] = useState('');
+  const [birthdayErr, setBirthdayErr] = useState('');
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
