8b032ce29763c67e4b320f44753214288a02893a
ibluehoodie <42532938+ibluehoodie@users.noreply.github.com>
2022-06-05 21:54:52 -0400

    Edit onLoggedOut() for logout button

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export function NavbarView({ user }) {
 
@@ -34,7 +35,7 @@ export function NavbarView({ user }) {
               <Nav.Link href={`/users/$(user)`}>{user}</Nav.Link>
             )}
             {isAuth() && (
-              <Button variant="link" onClick={() => { this.onLoggedOut() }}>Logout</Button>
+              <Button variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
             )}
             {!isAuth() && (
               <Nav.Link href="/">Sign-in</Nav.Link>
